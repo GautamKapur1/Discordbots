d8413018c15fadf415db444f6eba672d0857b828
Gautam Kapur <gautkapur@gmail.com>
2023-02-16 21:39:10 -0500

    weather bot set up. Linked via api tokens.

--- weather_bot/bot.js
+++ weather_bot/bot.js
@@ -1,11 +1,16 @@
 require('dotenv').config(); //initialize dotenv
-const Discord = require('discord.js'); //import discord.js
-
-const client = new Discord.Client(); //create new client
+const { Client, GatewayIntentBits } = require('discord.js')
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        //GatewayIntentBits.Message,
+        //Intent is to message in the main chat
+    ]
+})
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 //make sure this line is the last line
-client.login(process.env.CLIENT_TOKEN); //login bot using token
+client.login(process.env.WEATHER_BOT_BOT_TOKEN); //login bot using token
