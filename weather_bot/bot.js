8dc2c2bf5e507295d232d101a1e2000b17fe5c04
Gautam Kapur <gautkapur@gmail.com>
2023-04-05 23:24:48 -0400

    added basic message create command

--- weather_bot/bot.js
+++ weather_bot/bot.js
@@ -3,6 +3,8 @@ const { Client, GatewayIntentBits } = require('discord.js')
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMessages,
         //GatewayIntentBits.Message,
         //Intent is to message in the main chat
     ]
@@ -10,6 +12,14 @@ const client = new Client({
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('messageCreate', (message) => {
+  console.log(message);
+  if (message.author.bot) {
+    return;
+  }
+  message.reply("Hello " + message.author.username + "!");
 });
 
 //make sure this line is the last line
